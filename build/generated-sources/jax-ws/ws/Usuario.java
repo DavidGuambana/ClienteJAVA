a8e07460ff2820d67a218b29a776965a1f4665e7
DavidGuambana <112259062+DavidGuambana@users.noreply.github.com>
2024-05-10 18:23:38 -0500

    formularios del cliente

---
+++ build/generated-sources/jax-ws/ws/Usuario.java
@@ -0,0 +1,114 @@
+
+package ws;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Clase Java para usuario complex type.
+ * 
+ * <p>El siguiente fragmento de esquema especifica el contenido que se espera que haya en esta clase.
+ * 
+ * <pre>
+ * &lt;complexType name="usuario"&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
+ *       &lt;sequence&gt;
+ *         &lt;element name="clave" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/&gt;
+ *         &lt;element name="dinero" type="{http://www.w3.org/2001/XMLSchema}double" minOccurs="0"/&gt;
+ *         &lt;element name="usuario" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/&gt;
+ *       &lt;/sequence&gt;
+ *     &lt;/restriction&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "usuario", propOrder = {
+    "clave",
+    "dinero",
+    "usuario"
+})
+public class Usuario {
+
+    protected String clave;
+    protected Double dinero;
+    protected String usuario;
+
+    /**
+     * Obtiene el valor de la propiedad clave.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getClave() {
+        return clave;
+    }
+
+    /**
+     * Define el valor de la propiedad clave.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setClave(String value) {
+        this.clave = value;
+    }
+
+    /**
+     * Obtiene el valor de la propiedad dinero.
+     * 
+     * @return
+     *     possible object is
+     *     {@link Double }
+     *     
+     */
+    public Double getDinero() {
+        return dinero;
+    }
+
+    /**
+     * Define el valor de la propiedad dinero.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link Double }
+     *     
+     */
+    public void setDinero(Double value) {
+        this.dinero = value;
+    }
+
+    /**
+     * Obtiene el valor de la propiedad usuario.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getUsuario() {
+        return usuario;
+    }
+
+    /**
+     * Define el valor de la propiedad usuario.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setUsuario(String value) {
+        this.usuario = value;
+    }
+
+}
